8c602d470f8b4f2462cf46dbb448574bacd9f96b
Vatsal Khatri <201951081@iiitvadodara.ac.in>
2023-03-26 00:58:08 +0530

    Made few changes

---
+++ pizza-project/src/components/Menu.js
@@ -0,0 +1,14 @@
+import React from "react";
+const Menu = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+      }}
+    >
+      <p className="top-title">Welcome to Vatsal's pizza</p>
+      <hr className="hr1" />
+    </div>
+  );
+};
+export default Menu;
